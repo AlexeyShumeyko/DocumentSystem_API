207af02a71073c4bbc80c6b6945db73d149ef756
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 22:30:11 +0300

    Creating a service for changing document statuses depending on task completion

---
+++ DocumentSystem.Application/Interfaces/ITaskService.cs
@@ -0,0 +1,9 @@
+﻿namespace DocumentSystem.Application.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<bool> CompleteTaskAsync(string documentId);
+        Task<bool> CancelDocumentAsync(string documentId);
+
+    }
+}
---
+++ DocumentSystem.Application/Services/TaskService.cs
@@ -0,0 +1,70 @@
+﻿using DocumentSystem.Application.Interfaces;
+using DocumentSystem.Core.Contract;
+using DocumentSystem.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentSystem.Application.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TaskService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> CompleteTaskAsync(string documentId)
+        {
+            var documentRepo = _unitOfWork.GetRepository<DocumentEntity>();
+
+            var document = await documentRepo
+                .AsQueryable()
+                .FirstOrDefaultAsync(d => d.Id == documentId);
+
+            if (document == null)
+                return false;
+
+            var activeTask = document.ActiveTask;
+
+            if (activeTask != null)
+                return false;
+
+            var nextTask = document.Tasks
+                .FirstOrDefault(t => t.PreviousTaskID == activeTask!.Id);
+
+            if (nextTask != null) 
+            {
+                document.ActiveTask = nextTask;
+            }
+            else
+            {
+                document.Status = "В оперативном архиве";
+                document.ActiveTask = null;
+            }
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> CancelDocumentAsync(string documentId)
+        {
+            var documentRepo = _unitOfWork.GetRepository<DocumentEntity>();
+
+            var document = await documentRepo
+                .AsQueryable()
+                .FirstOrDefaultAsync(d => d.Id == documentId);
+
+            if (document == null)
+                return false;
+
+            document.Status = "Отмена";
+            document.ActiveTask = null;
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
