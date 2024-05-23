a3bdc8fccd17a6cbc9e2eccce1d58927a838876e
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 13:59:15 +0300

    Creating and config DbContext

---
+++ DocumentSystem.DAL/DocumentSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DocumentSystem.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace DocumentSystem.DAL
+{
+    public class DocumentSystemDbContext(
+        DbContextOptions<DocumentSystemDbContext> options)
+        : DbContext(options)
+    {
+        public DbSet<DocumentEntity> Documents { get; set; }
+
+        public DbSet<TaskEntity> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DocumentSystemDbContext).Assembly);
+        }
+    }
+}
