bf7ef99d1ddd39e7c2f1d842974130153fca4e2c
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 14:01:31 +0300

    Applying the generic Repository pattern

---
+++ DocumentSystem.Core/Contract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace DocumentSystem.Core.Contract
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Create(TEntity entity);
+
+        TEntity Delete(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        IQueryable<TEntity> AsQueryable();
+    }
+}
---
+++ DocumentSystem.DAL/Repository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DocumentSystem.Core.Contract;
+
+namespace DocumentSystem.DAL
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> AsQueryable()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public TEntity Create(TEntity entity)
+        {
+            return _dbSet.Add(entity).Entity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            return _dbSet.Update(entity).Entity;
+        }
+
+        public TEntity Delete(TEntity entity)
+        {
+            return _dbSet.Remove(entity).Entity;
+        }
+    }
+}
