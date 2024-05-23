546213af12ec3472faf7175615509cb72d33560c
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 14:01:45 +0300

    Applying the Unit Of Work pattern

---
+++ DocumentSystem.Core/Contract/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Security.Principal;
+
+namespace DocumentSystem.Core.Contract
+{
+    public interface IUnitOfWork
+    {
+        IDbContextTransaction BeginTransaction();
+
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ DocumentSystem.DAL/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using DocumentSystem.Core.Contract;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentSystem.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+        private IDbContextTransaction _currentTransaction;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IDbContextTransaction BeginTransaction()
+        {
+            lock (_dbContext)
+            {
+                if (_currentTransaction != null)
+                {
+                    throw new Exception();
+                }
+
+                _currentTransaction = _dbContext.Database.BeginTransaction();
+            }
+            return _currentTransaction;
+        }
+
+        IRepository<TEntity> IUnitOfWork.GetRepository<TEntity>()
+        {
+            return new Repository<TEntity>(_dbContext);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+    }
+}
