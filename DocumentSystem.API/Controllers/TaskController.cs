7542b0bdb1cd211b1157f9110e331f53b6af498e
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 22:31:51 +0300

    Creating a controller to process requests for changes in document statuses

---
+++ DocumentSystem.API/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using DocumentSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentSystem.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> CompleteTask(int id)
+        {
+            var result = await _taskService.CompleteTaskAsync(id);
+
+            if (!result)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> CancelDocument(int id)
+        {
+            var result = await _taskService.CancelDocumentAsync(id);
+
+            if (!result) 
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
