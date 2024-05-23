3dd84a343a1d800679c1bca5b8b85bcab59af09f
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 22:24:26 +0300

    Creating a service to manage CRUD operations on documents

---
+++ DocumentSystem.Application/Interfaces/IDocumentService.cs
@@ -0,0 +1,12 @@
+﻿using DocumentSystem.Core.Model;
+
+namespace DocumentSystem.Application.Interfaces
+{
+    public interface IDocumentService
+    {
+        List<DocumentEntity> GetDocuments();
+        Task<DocumentEntity> GetDocumentByIdAsync(string documentId);
+        Task<DocumentEntity> CreateDocumentAsync(DocumentEntity document);
+        Task<bool> DeleteDocumentAsync(string documentId);
+    }
+}
---
+++ DocumentSystem.Application/Services/DocumentService.cs
@@ -0,0 +1,66 @@
+﻿using DocumentSystem.Application.Interfaces;
+using DocumentSystem.Core.Contract;
+using DocumentSystem.Core.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentSystem.Application.Services
+{
+    public class DocumentService : IDocumentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DocumentService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<DocumentEntity> GetDocuments()
+        {
+            var documentRepo = _unitOfWork.GetRepository<DocumentEntity>();
+
+            var documents = documentRepo.AsQueryable().ToList();
+
+            return documents;
+        }
+
+        public async Task<DocumentEntity> GetDocumentByIdAsync(string documentId)
+        {
+            var documentRepo = _unitOfWork.GetRepository<DocumentEntity>();
+
+            var document = await documentRepo
+                .AsQueryable()
+                .FirstOrDefaultAsync(d => d.Id == documentId);
+
+            return document;
+        }
+
+        public async Task<DocumentEntity> CreateDocumentAsync(DocumentEntity document)
+        {
+            var documentRepo = _unitOfWork.GetRepository<DocumentEntity>();
+
+            documentRepo.Create(document);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return document;
+        }
+
+        public async Task<bool> DeleteDocumentAsync(string documentId)
+        {
+            var documentRepo = _unitOfWork.GetRepository<DocumentEntity>();
+
+            var document = await documentRepo
+                .AsQueryable()
+                .FirstOrDefaultAsync(d => d.Id == documentId);
+
+            if (document == null)
+                return false;
+
+            documentRepo.Delete(document);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
