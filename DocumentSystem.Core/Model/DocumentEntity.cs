cd53ca5ab98927a7328af05e1cdd202efe65b3cb
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-23 13:55:15 +0300

    Adding the main document and task entities

---
+++ DocumentSystem.Core/Model/DocumentEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace DocumentSystem.Core.Model
+{
+    public class DocumentEntity
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+
+        public TaskEntity ActiveTask { get; set; }
+        public List<TaskEntity> Tasks { get; set; }
+    }
+}
---
+++ DocumentSystem.Core/Model/TaskEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace DocumentSystem.Core.Model
+{
+    public class TaskEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PreviousTaskID { get; set; }
+    }
+}
