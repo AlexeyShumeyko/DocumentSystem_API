9bfa80246e2eeeedd46f93211b3f3c71a63f95cc
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-24 14:00:02 +0300

    applying migrations

---
+++ DocumentSystem.DAL/Migrations/20240524102419_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    ActiveTaskId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    PreviousTaskID = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Documents_PreviousTaskID",
+                        column: x => x.PreviousTaskID,
+                        principalTable: "Documents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_ActiveTaskId",
+                table: "Documents",
+                column: "ActiveTaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_PreviousTaskID",
+                table: "Tasks",
+                column: "PreviousTaskID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Documents_Tasks_ActiveTaskId",
+                table: "Documents",
+                column: "ActiveTaskId",
+                principalTable: "Tasks",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Documents_Tasks_ActiveTaskId",
+                table: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+
+            migrationBuilder.DropTable(
+                name: "Documents");
+        }
+    }
+}
---
+++ DocumentSystem.DAL/Migrations/20240524102731_ref.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class @ref : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Documents_PreviousTaskID",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PreviousTaskID",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Documents_PreviousTaskID",
+                table: "Tasks",
+                column: "PreviousTaskID",
+                principalTable: "Documents",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Documents_PreviousTaskID",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PreviousTaskID",
+                table: "Tasks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Documents_PreviousTaskID",
+                table: "Tasks",
+                column: "PreviousTaskID",
+                principalTable: "Documents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
