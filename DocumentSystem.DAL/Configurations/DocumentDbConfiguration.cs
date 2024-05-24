7eaa6ed01b95d50b8dbbedd6ab0995e8ec3da75c
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-24 13:59:42 +0300

    adding DB configuration

---
+++ DocumentSystem.DAL/Configurations/DocumentDbConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DocumentSystem.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DocumentSystem.DAL.Configurations
+{
+    public class DocumentDbConfiguration : IEntityTypeConfiguration<DocumentEntity>
+    {
+        public void Configure(EntityTypeBuilder<DocumentEntity> builder) 
+        {
+            builder.HasKey(d => d.Id);
+            builder.Property(d => d.Id).IsRequired();
+            builder.Property(d => d.Status).IsRequired();
+
+            builder.HasOne(x => x.ActiveTask);
+
+            builder.HasMany(x => x.Tasks).WithOne().HasForeignKey(x => x.PreviousTaskID);
+
+        }
+    }
+}
---
+++ DocumentSystem.DAL/Configurations/TaskDbConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DocumentSystem.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DocumentSystem.DAL.Configurations
+{
+    public class TaskDbConfiguration : IEntityTypeConfiguration<TaskEntity>
+    {
+        public void Configure(EntityTypeBuilder<TaskEntity> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Id).IsRequired();
+            builder.Property(t => t.Name).IsRequired();
+
+            builder.Property(t => t.PreviousTaskID).IsRequired(false);
+        }
+    }
+}
