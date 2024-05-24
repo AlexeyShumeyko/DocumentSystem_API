a984563a61e3971287e0e871e7c55fc375f2ccd9
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-05-24 14:02:09 +0300

    Database registration; adding services to DI

--- DocumentSystem.API/Program.cs
+++ DocumentSystem.API/Program.cs
@@ -1,8 +1,12 @@
+using DocumentSystem.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Startup.AddServices(builder);
 
 var app = builder.Build();
 
---
+++ DocumentSystem.API/Startup.cs
@@ -0,0 +1,48 @@
+﻿using DocumentSystem.Application.Interfaces;
+using DocumentSystem.Application.Services;
+using DocumentSystem.Core.Contract;
+using DocumentSystem.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentSystem.API
+{
+    public static class Startup
+    {
+        internal static void AddServices(WebApplicationBuilder builder) 
+        { 
+            RegisterDAL(builder);
+
+            RegisterService(builder.Services);
+        }
+
+        private static void RegisterDAL(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("Sqlite");
+
+            builder.Services.AddTransient(provider =>
+            {
+                var builder = new DbContextOptionsBuilder<DocumentSystemDbContext>();
+
+                builder.UseSqlite(connectionString);
+
+                return builder.Options;
+            });
+
+            builder.Services.AddScoped<DbContext, DocumentSystemDbContext>();
+
+            builder.Services.AddScoped<IUnitOfWork>(prov =>
+            {
+                var context = prov.GetRequiredService<DbContext>();
+
+                return new UnitOfWork(context);
+            });
+        }
+
+        private static void RegisterService(IServiceCollection services)
+        {
+            services.AddScoped<IDocumentService, DocumentService>();
+
+            services.AddScoped<ITaskService, TaskService>();
+        }
+    }
+}
